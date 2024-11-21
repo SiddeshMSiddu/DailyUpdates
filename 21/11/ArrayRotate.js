a0c10691182636021267fdc6427e3149a340213a
Siddesh4877 <siddeshm4877@gmail.com>
2024-11-21 14:27:02 +0530

    ArrayRotate

---
+++ 21/11/ArrayRotate.js
@@ -0,0 +1,13 @@
+function ArrayRotate(arr,steps){
+    let res=[];
+    let n=arr.length;
+
+    steps=steps%n;
+    for(let i=0;i<arr.length;i++){
+        let newIndex=(i+steps)%n;
+        res[i]=arr[newIndex];
+    }
+    return res;
+}
+let arr=[1,2,3,4,5];
+console.log(ArrayRotate(arr,2));
