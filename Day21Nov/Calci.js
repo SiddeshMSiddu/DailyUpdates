d55b3ad672929297b0956a3e659a252ded645975
Siddesh4877 <siddeshm4877@gmail.com>
2024-11-21 14:36:41 +0530

    Day2

---
+++ Day21Nov/Calci.js
@@ -0,0 +1,21 @@
+function calci(oper1,op,oper2){
+    switch(op){
+        case '+':console.log(oper1+oper2);
+                break;
+        case '-':console.log(oper1-oper2);
+                break;
+        case '*':console.log(oper1*oper2);
+                break;
+        case '/':console.log(oper1/oper2);
+                break;
+        case '%':console.log(oper1%oper2);
+                break;
+    }
+}
+
+calci(10,'/',2);
+calci(10,'+',2);
+calci(10,'*',2);
+calci(10,'-',2);
+calci(10,'%',2);
+calci(10,'%',3);
