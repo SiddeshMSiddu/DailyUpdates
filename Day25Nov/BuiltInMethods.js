06a6e68acca524a6a061919adc5d8d2c6f209894
Siddesh4877 <siddeshm4877@gmail.com>
2024-11-25 12:36:50 +0530

    BuiltInStringMethods

---
+++ Day25Nov/BuiltInMethods.js
@@ -0,0 +1,26 @@
+//  let str="Hello World"
+// console.log(str.charAt(6));
+// console.log(str.charCodeAt(2));
+
+// let arr=[2,4,6,-2,-4]
+// let even=arr.every((arr)=>arr%2==0)
+// console.log(even);
+
+// let str1="Hello world"
+// let str2=" World...!"
+// let str3=" Good Morning"
+// let str4=" Team"
+// console.log(str1.concat(str2,str3,str4));
+// console.log(str1.includes('lo'));
+// console.log(str1.indexOf('w'));
+// console.log(str1.slice(1,7));
+// console.log(str1.split(' '));
+// console.log(str1.lastIndexOf('l'));
+// console.log(str1.match('d'));
+// console.log(str1.replaceAll('l','g'));
+// console.log(str1.replace('l','g'));
+// console.log(str1.substring(1,7));
+let str=" hello world ";
+console.log(str.trimEnd());
+
+
