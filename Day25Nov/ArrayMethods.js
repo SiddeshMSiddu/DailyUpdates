ec7b103176a076277c707a58bb2d6490fa94a50d
Siddesh4877 <siddeshm4877@gmail.com>
2024-11-25 15:56:05 +0530

    Array methods

---
+++ Day25Nov/ArrayMethods.js
@@ -0,0 +1,15 @@
+// let arr=[1,2,3,4,5]
+// arr.shift(0);
+// console.log(arr);
+// arr.forEach(x=>console.log(x));
+// console.log(arr.find(x=>x>3));
+
+let arr=[5,1,11,9,10,4,1,6]
+let arr1=[10,20,30,40,50]
+let arr2=[100,200,300,400]
+// arr.sort()
+// console.log(arr);
+// console.log(arr.reverse());
+// console.log(arr.concat(arr1,arr2));
+// console.log(arr.find(x=>x>6));
+console.log(arr.join(", "));
