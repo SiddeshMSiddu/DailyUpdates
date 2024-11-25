bb369af62b32b6bffd049f2c5e3d521785f52990
Siddesh4877 <siddeshm4877@gmail.com>
2024-11-25 17:09:26 +0530

    Harshed Number

---
+++ Day25Nov/HarshadNumber.js
@@ -0,0 +1,13 @@
+function HarshedNum(num){
+    let sum=0;
+    let temp=num;
+    while(temp>0){
+        let rem=temp%10;
+        sum+=rem;
+        temp=Math.floor(temp/10);
+    }
+    console.log(num%sum==0? "Harshed Num":"Not Harshed");
+    
+}
+HarshedNum(18);
+HarshedNum(81)
