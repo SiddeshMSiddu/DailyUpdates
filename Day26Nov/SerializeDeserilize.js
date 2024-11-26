b92e3d9548cd494cae62a78fba08cc8b27306a4b
Siddesh4877 <siddeshm4877@gmail.com>
2024-11-26 14:27:30 +0530

    serilize deserilize

---
+++ Day26Nov/SerializeDeserilize.js
@@ -0,0 +1,7 @@
+// Serialization
+// Dserialization
+let user={"name":"Anand","age":52}
+let seriliz=JSON.stringify(user);
+console.log(seriliz);
+let deserilize=JSON.parse(seriliz);
+console.log(deserilize);
