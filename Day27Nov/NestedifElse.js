3980c7ff89ef51092eaa417e853a8c85f570c9a9
Siddesh4877 <siddeshm4877@gmail.com>
2024-11-27 11:27:27 +0530

    Greatest of 3 numbers

---
+++ Day27Nov/NestedifElse.js
@@ -0,0 +1,16 @@
+let a=50;
+let b=55;
+let c=45;
+if(a>b){
+    if(a>c){
+        console.log("a is greatest");
+    }else{
+        console.log("c is greatest");
+    }
+} else{
+    if(b>c){
+        console.log("b is greatest");
+    }else{
+        console.log("c is greatest");
+    }
+}
